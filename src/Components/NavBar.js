11fcc23511a1a193ded179976bf10b52b35d2f1f
Derrick <djmulinde@gmail.com>
2022-11-25 17:32:25 +0300

    Update navigation component

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -2,30 +2,22 @@ import React from 'react';
 import 'font-awesome/css/font-awesome.min.css';
 import logo from '../images/logo_crypto.png';
 
-const NavBar = () => {
-  const handleSearch = (e) => {
-    console.log(e.target.value);
-  };
-
-  return (
-    <header className="header flex">
-      <div className="logo-header flex">
-        <div className="logo flex">
-          <span className="back">
-            {/* <i className="fa fa-chevron-left" aria-hidden="true" /> */}
-            <span className="back-txt white"><img src={logo} alt="Logo" className="logo" /></span>
-          </span>
-        </div>
-      </div>
-      <div className="right-side flex">
-        <input type="search" placeholder="Search Cryptos" onChange={handleSearch} className="search-field" />
-        <span className="audio"><i className="fa fa-microphone" aria-hidden="true" /></span>
-        <span className="settings">
-          <i className="fa fa-cog" aria-hidden="true" />
+const NavBar = () => (
+  <header className="header flex">
+    <div className="logo-header flex">
+      <div className="logo flex">
+        <span className="back">
+          <span className="back-txt white"><img src={logo} alt="Logo" className="logo" /></span>
         </span>
       </div>
-    </header>
-  );
-};
+    </div>
+    <div className="right-side flex">
+      <span className="audio"><i className="fa fa-microphone" aria-hidden="true" /></span>
+      <span className="settings">
+        <i className="fa fa-cog" aria-hidden="true" />
+      </span>
+    </div>
+  </header>
+);
 
 export default NavBar;
